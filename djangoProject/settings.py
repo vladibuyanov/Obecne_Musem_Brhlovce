c98b20cf3f615363e7c598befd4f767597626103
Vladi <vladibuyanov@gmail.com>
2022-05-24 10:18:58 +0200

    first commit on Heroku

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -13,8 +13,6 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-from django.contrib import staticfiles
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -122,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'staticfiles'),
+   os.path.join(BASE_DIR, 'static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
